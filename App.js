7a7dc3db63c00fe6232a0ec93b2eb8c0d8c51690
willi301 <will301will@gmail.comm>
2025-06-04 23:46:05 -0700

    added basic ui

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Button, View, Text, Alert, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Add a Tip?</Text>
+
+      {/* this is the buttons for the tip options */}
+      <View style={styles.option}>
+        <TouchableOpacity
+          // onPress={onPressLearnMore}
+          style={styles.button}
+          color="cornflowerblue"
+        >
+          <Text style={styles.buttonText}>10%</Text>
+        </TouchableOpacity>
+          
+        <TouchableOpacity
+          // onPress={onPressLearnMore}
+          style={styles.button}
+          color="cornflowerblue"
+        >
+          <Text style={styles.buttonText}>20%</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          // onPress={onPressLearnMore}
+          style={styles.button}
+          color="cornflowerblue"
+        >
+          <Text style={styles.buttonText}>25%</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.other}>
+        <TouchableOpacity
+          // onPress={onPressLearnMore}
+          style={styles.otherButton}
+          color="cornflowerblue">
+            <Text style={styles.buttonText}>Others</Text>
+        </TouchableOpacity>
+      </View>
+
+
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +57,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+
+  option: {
+    width: "100%",
+    height: 200,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 0,
+    margin: 0,
+  },
+
+  button: {
+    width: 150,         // 👈 Width of the button
+    height: 50,         // 👈 Height of the button
+    backgroundColor: '#007BFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,       // 👈 Font size of the button label
+  },
+
+  other: {
+    width: "100%",
+    height: 100,
+    alignItems: 'center',
+  },
+
+  otherButton: {
+    width: "80%",
+    height: 50,
+    backgroundColor: '#007BFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
